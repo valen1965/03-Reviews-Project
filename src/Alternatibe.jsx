ac911fc05a51800690486c7ccd2882a331381c0b
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2023-08-26 02:26:22 +0300

    Reviews-Project Vesrion 1

---
+++ src/Alternatibe.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex > people.length - 1 ? 0 : newIndex;
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex < 0 ? people.length - 1 : newIndex;
+    });
+  };
+
+  const randomPerson = () => {
+    const randomIndex = Math.floor(Math.random() * people.length);
+    setIndex(randomIndex);
+    return;
+  };
+
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} alt={name} className='person-img' />
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button
+            className='prev-btn'
+            onClick={() => {
+              prevPerson();
+            }}
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className='next-btn'
+            onClick={() => {
+              nextPerson();
+            }}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          className='btn'
+          onClick={() => {
+            randomPerson();
+          }}
+        >
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
+};
+export default App;
